42e2907818b9305232e250cbc4407d4005ce93d0
Lucas Paviotti <lucas.paviotti@elpixel.com>
2022-04-28 22:32:31 -0300

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,6 +8,7 @@ import { ProductList } from './components/ProductList';
 import { GET_PRODUCTS } from './graphql/queries';
 import { ThemeProvider } from 'styled-components';
 import { Theme } from './components/styles/Theme.styled';
+import { ProductCard } from './components/ProductCard';
 
 const mockProducts: any[] = [
   {
@@ -53,5 +54,27 @@ describe('ProductList', () => {
     );
     await waitForElementToBeRemoved(() => screen.getByAltText('Spinner'));
     await new Promise((resolve) => setTimeout(resolve, 0));
+  });
+  it('renders product card', async () => {
+    render(
+      <ThemeProvider theme={Theme}>
+        <MockedProvider mocks={mockProducts} addTypename={false}>
+            <ProductCard 
+              key={mockProducts[0].result.data.products.items[0].id}
+              id={mockProducts[0].result.data.products.items[0].id}
+              description={mockProducts[0].result.data.products.items[0].description}
+              name={mockProducts[0].result.data.products.items[0].name}
+              assets={mockProducts[0].result.data.products.items[0].assets}
+              variants={mockProducts[0].result.data.products.items[0].variants}
+            />
+        </MockedProvider>
+      </ThemeProvider>
+    );
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    screen.getByAltText('Laptop')
+    screen.getAllByText(/Laptop/i)
+    screen.getByText(/Intel/i)
+    screen.getByText('$139,900.00')
+    screen.debug()
   });
 });
